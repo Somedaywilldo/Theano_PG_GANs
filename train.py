2ee9bff8d67b98c9cf3138937f56ab280d7b5d7c
Somedaywilldo <somedaywilldo@foxmail.com>
2018-03-09 10:15:39 +0800

    test2

--- train.py
+++ train.py
@@ -107,8 +107,8 @@ def train_gan(
     image_snapshot_ticks    = 4,
     network_snapshot_ticks  = 40,
     image_grid_type         = 'default',
-    resume_network_pkl      = None,
-    resume_kimg             = 0.0,
+    resume_network_pkl      = '006-celeb128-progressive-growing/network-snapshot-002009.pkl',
+    resume_kimg             = 2059.8,
     resume_time             = 0.0):
 
     # Load dataset and build networks.
